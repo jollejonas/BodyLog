dad12b53ea8941a0d85e906f095fbae8c3283f3a
jollejonas <jollevb@hotmail.com>
2025-07-29 22:53:32 +0200

    updated api route

--- bodylog-frontend/src/api/axios.js
+++ bodylog-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:51940/api',
+    baseURL: 'bodylog-api-e9eabfb0fpe8bpeg.westeurope-01.azurewebsites.net/api',
     headers: {
         'Content-Type': 'application/json',
     }
